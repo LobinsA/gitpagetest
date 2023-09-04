4e6fb761c2ffa094839fb5db8906c1952a98a2c3
DESKTOP-U0LNAI4\Personal <LobinsA@Yahoo.com>
2023-09-03 22:04:18 -0700

    test 2

--- getGraphQLData.js
+++ getGraphQLData.js
@@ -1,8 +1,8 @@
 const storedAccessToken = sessionStorage.getItem('accessToken');
 const outputElement = document.getElementById('output');
-outputElement.textContent = "Nope.";
+
 if(storedAccessToken) {
-     outputElement.textContent = storedAccessToken;
+    outputElement.textContent = storedAccessToken;
 } else {
     outputElement.textContent = "Nope.";
 }
