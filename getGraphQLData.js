dc7050eef6409ec61c3d8e898172d216541edad0
DESKTOP-U0LNAI4\Personal <LobinsA@Yahoo.com>
2023-09-03 23:58:31 -0700

    making a query to graphql public url

--- getGraphQLData.js
+++ getGraphQLData.js
@@ -1,3 +1,5 @@
+const publicUrl = 'https://www.warcraftlogs.com/oauth/token';
+
 try {
     const storedAccessToken = sessionStorage.getItem('accessToken');
     const outputElement = document.getElementById('output');
@@ -10,3 +12,44 @@ if(storedAccessToken) {
 } catch (error) {
     outputElement.textContent = "Fail!";
 }
+
+//todo: create queries for graphql and return certain data
+const graphqlQuery = `
+  query {
+    player(name: "YourPlayerName") {
+      combat(characterID: "YourCharacterID") {
+        damageTaken {
+          total
+          details {
+            ability {
+              name
+            }
+            total
+          }
+        }
+      }
+    }
+  }
+`;
+
+const headers = {
+    'Authorization': `Bearer ${storedAccessToken}`,
+    'Content-Type': 'application/json',
+};
+
+// Create the request options
+const requestOptions = {
+  method: 'POST',
+  headers: headers,
+  body: JSON.stringify({ query: graphqlQuery }),
+};
+
+// Make the GraphQL API call
+fetch(publicUrl, requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
