b6c395b5493a07b1a8651f3884c0eee811300444
DESKTOP-U0LNAI4\Personal <LobinsA@Yahoo.com>
2023-09-04 00:17:00 -0700

    fix

--- getGraphQLData.js
+++ getGraphQLData.js
@@ -5,45 +5,44 @@ try {
     const outputElement = document.getElementById('output');
     const dataElement = document.getElementById('sci');
 
-if(storedAccessToken) {
-    outputElement.textContent = "Success!";
-} else {
-    outputElement.textContent = "Fail!";
-}
+    if(storedAccessToken) {
+        outputElement.textContent = "Success!";
+        //todo: create queries for graphql and return certain data
+        const graphqlQuery = `
+        query{
+            characterData{
+                character(name:"Sci",serverSlug:"Area-52",serverRegion:"US"){
+                    level
+                    classID
+                    name
+                }
+        }
+        `;
+
+        const headers = {
+            'Authorization': `Bearer ${storedAccessToken}`,
+            'Content-Type': 'application/json',
+        };
+
+        // Create the request options
+        const requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({ query: graphqlQuery }),
+        };
+
+        // Make the GraphQL API call
+        fetch(publicUrl, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            dataElement.textContent(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    } else {
+        outputElement.textContent = "Fail!";
+    }
 } catch (error) {
-    outputElement.textContent = "Fail!";
+        outputElement.textContent = "Fail!";
 }
-
-//todo: create queries for graphql and return certain data
-const graphqlQuery = `
-query{
-	characterData{
-		character(name:"Sci",serverSlug:"Area-52",serverRegion:"US"){
-			level
-			classID
-			name
-		}
-}
-`;
-
-const headers = {
-    'Authorization': `Bearer ${storedAccessToken}`,
-    'Content-Type': 'application/json',
-};
-
-// Create the request options
-const requestOptions = {
-  method: 'POST',
-  headers: headers,
-  body: JSON.stringify({ query: graphqlQuery }),
-};
-
-// Make the GraphQL API call
-fetch(publicUrl, requestOptions)
-  .then(response => response.json())
-  .then(data => {
-    dataElement.textContent(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
